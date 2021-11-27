91d679509ffb707ac0ca09772b51a2e0f23a31b5
Eric Fäh <eric.faeh@stud.unibas.ch>
2021-11-27 19:32:56 +0100

    test

--- test.sol
+++ test.sol
@@ -7,8 +7,17 @@ pragma solidity ^0.8.9;
 contract Blackjack { // Blackjack parameters
   address public Player
   uint public endTime; // as UNIX timestamp for end of round
+// Time for each round 1 min need to include mutliple rounds
+  constructor (address _Player, uint _durationMinutes) {
+    Player = _Player;
+    endTime = block.timestamp + _durationMinutes * 1 minutes;
+  }
+  // accept and store valid bets
 
-  constructor (address _Player, uint )
+  function bet() public payable {
+    require (block.timestamp < endTime, 'No bets allowed'
+  );
+  }
 
   //State of the game
   uint  public playerbet;
