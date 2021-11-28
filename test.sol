fa86cd0b5b60cff7a8e77aa33f87bb7308db295e
Eric Fäh <eric.faeh@stud.unibas.ch>
2021-11-28 12:38:35 +0100

    test to upload

--- test.sol
+++ test.sol
@@ -1,28 +1,27 @@
+pragma solidity ^0.8.9;
+
 //SPDX-License-Identifier: MIT
 
 // Warning: Do not use for actual funds.
 
-pragma solidity ^0.8.9;
-
 contract Blackjack { // Blackjack parameters
-  address private Player = msg.sender
-  uint public endTime; // as UNIX timestamp for end of round
+  address private Player = msg.sender;
+  uint public TurnEndTime;// as UNIX timestamp for end of round
 // Time for each round 1 min need to include mutliple rounds
   constructor (address _Player, uint _durationMinutes) {
     Player = _Player;
-    endTime = block.timestamp + _durationMinutes * 1 minutes;
+    TurnEndTime = block.timestamp + _durationMinutes * 1 minutes;
   }
   // accept and store valid bets
 
   function bet() public payable {
-    require (block.timestamp < endTime, 'No bets allowed'
-  );
+    require (block.timestamp < TurnEndTime, 'No bets allowed');
   }
 
   //State of the game
-  uint  public playerbet;
+  uint public playerbet;
   address public winnerBlackjack;
-  bool public has Ended;
+  bool public hasEnded;
 
   function initialize (address _Player) external
   {
