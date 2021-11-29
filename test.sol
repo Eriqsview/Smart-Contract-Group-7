46ea3370b25550fa18721a1164832c8a07e5df23
Eriqsview <94555086+Eriqsview@users.noreply.github.com>
2021-11-29 16:07:37 +0100

    Update test.sol

--- test.sol
+++ test.sol
@@ -22,28 +22,13 @@ contract Blackjack { // Blackjack parameters
   uint public playerbet;
   address public winnerBlackjack;
   bool public hasEnded;
-
+  uint [13] cardValues = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
   function initialize (address _Player) external
   {
     Player = _Player;
   }
 }
-contract deck {
-    enum suit { club, diamonds, hearts, spades }
-    enum value { two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace }
-    struct card {
-        value v;
-        suit s;
-    }
 
-    card card1 = card(value.two, suit.club);
-    card card2 = card({s: suit.club, v: value.two});
-
-    // This function does a lot of copying
-    function set_card1(card c) public returns (card previous) {
-        previous = card1;
-        card1 = c;
-    }
 }
 address public player;
 
